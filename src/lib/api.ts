ab67ae14d928704bf67b07c29af6a6e98e6450fd
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-16 22:29:17 -0300

    feat: add configurations of SSR and SSG

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,25 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 export async function getPosts() {
-  return fetch(`${BASE_URL}/posts`).then((res) => res.json());
+  return fetch(`${BASE_URL}/posts`, {
+    cache: "no-store", //SSR
+  }).then((res) => res.json());
 }
 
 export async function getPost(id: string) {
-  return fetch(`${BASE_URL}/posts/${id}`).then((res) => res.json());
+  return fetch(`${BASE_URL}/posts/${id}`, {
+    next: { revalidate: 60 * 60 }, // 1 hour
+  }).then((res) => res.json());
 }
 
 export async function getPostComments(postId: string) {
-  return fetch(`${BASE_URL}/posts/${postId}/comments`).then((res) =>
-    res.json()
-  );
+  return fetch(`${BASE_URL}/posts/${postId}/comments`, {
+    cache: "no-store", //SSR
+  }).then((res) => res.json());
 }
 
 export async function getUser(userId: string) {
-  return fetch(`${BASE_URL}/users/${userId}`).then((res) => res.json());
+  return fetch(`${BASE_URL}/users/${userId}`, {
+    cache: "no-store", //SSR
+  }).then((res) => res.json());
 }
