10ba13b7d9ce2474b1edf18eb640af2fb6506771
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-17 01:06:18 -0300

    feat: remove past code and add ListPostCards component on home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
-
 import { getPosts } from "@/lib/api";
-import PostProps from "@/types/post";
+import ListPostCards from "@/components/ListPostCards";
 
 export default async function HomePage() {
   const posts = await getPosts();
@@ -11,22 +9,7 @@ export default async function HomePage() {
       <div className="bg-white rounded-xl  p-6">
         <h1 className="text-3xl font-bold mb-5">The latest news</h1>
 
-        <div className="space-y-4">
-          {posts.map((post: PostProps) => (
-            <Link
-              key={post.id}
-              href={`/post/${post.id}`}
-              className="block border border-gray-200 rounded-md p-4 hover:bg-gray-50 transition"
-            >
-              <h2 className="text-lg font-semibold text-gray-900 mb-1">
-                {post.title}
-              </h2>
-              <p className="text-gray-600 text-sm">
-                {post.body.substring(0, 80)}...
-              </p>
-            </Link>
-          ))}
-        </div>
+        <ListPostCards posts={posts} customSubstring={140} />
       </div>
     </main>
   );
