5e479848e53a401bb5d8bf7cc8b6473b9a53cabb
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-17 02:31:28 -0300

    feat: add BackButton on user screen

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-
 import { getUser, getPosts } from "@/lib/api";
 import PostProps from "@/types/post";
 import getInitialsCharactersByName from "@/utils/getInitialsCharactersByName";
+import ListPostCards from "@/components/ListPostCards";
+import BackButton from "@/components/BackButton";
 
 interface UserPageProps {
   params: { id: string };
@@ -11,14 +11,16 @@ interface UserPageProps {
 export default async function UserPage({ params }: UserPageProps) {
   const user = await getUser(params?.id);
   const allPosts = await getPosts();
+
   const userPosts = allPosts.filter(
     (post: PostProps) => post.userId === parseInt(params?.id)
   );
 
   return (
-    <main className="max-w-3xl mx-auto px-4 py-12 text-white">
+    <main className="max-w-5xl mx-auto px-4 py-12 text-white">
+      <BackButton />
       <section className="flex flex-col items-center text-center mb-10">
-        <div className="text-black w-[100px] h-[100px] rounded-full bg-black/10 flex items-center justify-center text-3xl font-bold mx-auto md:mx-0 mb-4">
+        <div className="text-black w-[100px] h-[100px] rounded-full bg-[#6E9BFF] flex items-center justify-center text-3xl font-bold mx-auto md:mx-0 mb-4">
           {getInitialsCharactersByName(user.name)}
         </div>
         <h1 className="text-black text-3xl font-bold">{user.name}</h1>
@@ -28,24 +30,7 @@ export default async function UserPage({ params }: UserPageProps) {
         <h2 className="text-black  text-2xl font-semibold mb-6">
           {userPosts?.length} Posts
         </h2>
-        <div className="space-y-4">
-          {userPosts.map((post: PostProps) => (
-            <Link
-              href={`/post/${post.id}`}
-              className="block  border border-gray-200 rounded-md p-4  hover:bg-gray-50 transition"
-              key={post.id}
-            >
-              <div key={post.id}>
-                <h3 className=" text-black font-semibold text-lg mb-1">
-                  {post.title}
-                </h3>
-                <p className="text-gray-400 text-sm">
-                  {post.body.substring(0, 100)}...
-                </p>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <ListPostCards posts={allPosts} customSubstring={130} />
       </section>
     </main>
   );
