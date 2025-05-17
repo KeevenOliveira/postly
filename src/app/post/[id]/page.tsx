dd01dfaedb87539b44e0493c88cd35a6452ac99e
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-17 00:15:13 -0300

    feat: add types and more adjusts of user screen redirect

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import CommentProps from "@/types/comment";
 import { getPost, getUser, getPostComments } from "@/lib/api";
 
 interface PostPageProps {
@@ -5,7 +9,7 @@ interface PostPageProps {
 }
 
 export default async function PostPage({ params }: PostPageProps) {
-  const postId = params.id;
+  const postId = params?.id;
 
   const [post, comments] = await Promise.all([
     getPost(postId),
@@ -15,31 +19,41 @@ export default async function PostPage({ params }: PostPageProps) {
   const user = await getUser(post.userId);
 
   return (
-    <main className="max-w-2xl mx-auto px-4 py-10 bg-white rounded-xl shadow-md">
-      <h1 className="text-3xl font-bold mb-2">Postly</h1>
-      <p className="text-gray-500 mb-6">
-        A simple and <span className="font-semibold">elegant</span> blog reader
-        built with Next.js.
-      </p>
-
-      <hr className="mb-6" />
-
+    <main className="max-w-6xl mx-auto px-4 py-10 bg-white rounded-xl">
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-2">{post.title}</h2>
         <p className="text-gray-700">{post.body}</p>
-        <p className="mt-4 font-medium"> {user.name}</p>
+        <Link href={`/user/${user.id}`}>
+          <p className="mt-4 font-medium"> {user.name}</p>
+        </Link>
       </section>
 
       <section>
-        <h3 className="text-xl font-semibold mb-4">Comments</h3>
+        <h3 className="text-xl font-semibold mb-4">
+          {comments?.length} Comments
+        </h3>
         <div className="space-y-3">
-          {comments.map((comment) => (
+          {comments.map((comment: CommentProps) => (
             <div
               key={comment.id}
-              className="border border-gray-200 rounded-md p-3 bg-white text-gray-800"
+              className="flex items-start border border-gray-200 rounded-md p-3 bg-white text-gray-800"
             >
-              <p className="font-semibold">{comment.name}</p>
-              <p className="text-sm text-gray-600">{comment.body}</p>
+              <div>
+                <div className="w-8 h-8 rounded-full bg-blue-600 flex items-center justify-center text-xs font-bold text-white mr-3">
+                  <Image
+                    src="/comment/person.svg"
+                    alt="User"
+                    width={100}
+                    height={100}
+                    className="w-8 h-8 rounded-full"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <p className="font-semibold">{comment.name}</p>
+                <p className="text-sm text-gray-600">{comment.body}</p>
+              </div>
             </div>
           ))}
         </div>
