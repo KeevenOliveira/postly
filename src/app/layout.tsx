47ba4e90d1da8749d5d54bf3cfe85126a43fbb5b
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-17 00:12:34 -0300

    feat: delete getInitialsCharatectersByEmail function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
+
+import Header from "@/components/Header";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <div className="pt-16 min-h-screen">{children}</div>
+      </body>
     </html>
   );
 }
--- src/utils/getInitialsCharactersByEmail.ts
+++
@@ -1,6 +0,0 @@
-export default function getInitialsCharactersByEmail(email: string): string {
-  const words = email?.trim();
-  const first = words?.[0]?.trim() || "";
-  const second = words?.[1]?.trim() || "";
-  return (first + second)?.toUpperCase();
-}
