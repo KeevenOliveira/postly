d6e5d82a624f11e3110bf93ca15e26383ea38248
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-05-17 02:43:19 -0300

    feat: add optional values in getInitialsCharactersByName

--- src/utils/getInitialsCharactersByName.ts
+++ src/utils/getInitialsCharactersByName.ts
@@ -1,5 +1,5 @@
 export default function getInitialsCharactersByName(name: string): string {
-  const parts = name.trim().split(" ");
+  const parts = name?.trim()?.split(" ");
   const first = parts[0]?.[0]?.trim() || "";
   const second = parts[1]?.[0]?.trim() || "";
   return `${first}${second}`.toUpperCase();
